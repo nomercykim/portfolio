f9aba4ee9b20070035a7da2bd0d1d9f0bdd03ea5
nomercykim <id6word@gmail.com>
2023-11-02 20:15:26 +0900

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// 프로젝트 필터링 관련 로직 처리
+const categories  =  document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event)=>{
+    const filter = event.target.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    handleActvieSelection(event.target);
+    filterProjects(filter);
+})
+
+
+function handleActvieSelection(target){
+    const actvie = document.querySelector('.category--selected');
+    actvie.classList.remove('category--selected');
+    target.classList.add('category--selected');
+}
+
+function filterProjects(filter){
+    projectsContainer.classList.add('anim-out');
+    projects.forEach(project =>{
+        if(filter === 'all' || filter === project.dataset.type){
+            project.style.display ='block';      
+        }
+        else{
+            project.style.display ='none';
+        }
+    });
+    setTimeout(() => {
+        projectsContainer.classList.remove('anim-out');
+    }, 250);
+}
