29dac6a780736b604f0f2d9d0176d0649fb05347
nomercykim <id6word@gmail.com>
2023-11-09 23:07:57 +0900

    Divider

---
+++ src/type.js
@@ -0,0 +1,18 @@
+'use strict';
+
+new TypeIt('.home__title--strong', {loop:true, speed: 50,})
+.move(-11)
+.type('Amazing ')
+.pause(1000)
+.move(null, {to:'END'})
+.delete()
+.type('Front-end Engineer')
+.pause(1000)
+.move(-9)
+.delete(9)
+.type('Back-end')
+.pause(1000)
+.type('Full-stack')
+.pause(1000)
+.delete()
+.go();
